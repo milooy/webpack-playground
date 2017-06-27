c3197752e75f9f660928e10454d41b82438d90e5
jayjin <jayjinjay@gmail.com>
2017-06-27 18:01:47 +0900

    Asset management

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // CSS 파일을 js 모듈 안에서 import하기 위해서 씀. Asset관리
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  }
+};
